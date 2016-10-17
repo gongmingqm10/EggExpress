fdee25973e17e1a72af8a25b45fe1ada2692f768
mingong <gongmingqm10@foxmail.com>
2016-10-17 21:05:23 +0100

    List users in home page

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
+var fs = require('fs')
+var _ = require('lodash')
 var express = require('express')
 var app = express()
+var users = []
+
+fs.readFile('users.json', {encoding: 'utf8'}, function(err, data) {
+  if (err) throw err
+  JSON.parse(data).forEach(function(user) {
+    user.name.full = _.startCase(user.name.first + ' ' + user.name.last)
+    users.push(user)
+  })
+  console.log('users.count = ' + users.length)
+})
 
 app.get('/', function(req, res) {
-  res.send('Hello, Ming!')
+  var buffer = ''
+
+  users.forEach(function(user) {
+    buffer += '<a href = "' + user.username + '">' +  user.name.full + '</a><br>'
+  })
+
+  console.log("@@@@@@@@@@@@@" + buffer)
+  res.send(buffer)
 })
+
 
 var server = app.listen(3000, function() {
   console.log('Server running at http://localhost:' + server.address().port)
