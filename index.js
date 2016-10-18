972039824cf93df475d0166349fa36626d8b6e54
mingong <gongmingqm10@foxmail.com>
2016-10-18 14:16:33 +0100

    Add user delete function

--- index.js
+++ index.js
@@ -6,15 +6,6 @@ var engines = require('consolidate')
 var bodyParser = require('body-parser')
 
 var app = express()
-var users = []
-
-fs.readFile('users.json', {encoding: 'utf8'}, function(err, data) {
-  if (err) throw err
-  JSON.parse(data).forEach(function(user) {
-    user.name.full = _.startCase(user.name.first + ' ' + user.name.last)
-    users.push(user)
-  })
-})
 
 app.set('views', './views')
 
@@ -27,7 +18,19 @@ app.use('/profilepics', express.static('images'))
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', function(req, res) {
-  res.render('index', {users: users})
+  var users = []
+
+  fs.readdir('users', function(err, files) {
+    files.forEach(function(file) {
+      fs.readFile(path.join(__dirname, 'users', file), {encoding: 'utf8'}, function(err, data) {
+        var user = JSON.parse(data)
+        user.name.full = _.startCase(user.name.first + ' ' + user.name.last)
+        users.push(user)
+
+        if (users.length === files.length) res.render('index', {users: users})
+      })
+    })
+  })
 })
 
 app.get('/users/big*', function(req, res, next) {
@@ -47,6 +50,12 @@ app.put('/users/:username', function(req, res) {
   user.location = req.body
   saveUser(username, user)
   res.end()
+})
+
+app.delete('/users/:username', function(req, res) {
+  var fq = getUserFilePath(req.params.username)
+  fs.unlinkSync(fq)
+  res.sendStatus(200)
 })
 
 var server = app.listen(3000, function() {
