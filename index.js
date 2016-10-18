6a7a436acb150b1f99f95fb5881128b9b679c207
mingong <gongmingqm10@foxmail.com>
2016-10-18 12:05:24 +0100

    Add http verb to update user profile

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var fs = require('fs')
+var path = require('path')
 var _ = require('lodash')
 var express = require('express')
 var engines = require('consolidate')
+var bodyParser = require('body-parser')
 
 var app = express()
 var users = []
@@ -22,6 +24,8 @@ app.set('view engine', 'hbs')
 
 app.use('/profilepics', express.static('images'))
 
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.get('/', function(req, res) {
   res.render('index', {users: users})
 })
@@ -37,11 +41,36 @@ app.get('/users/:username', function(req, res) {
   res.render('user', {user: user, address: user.location})
 })
 
+app.put('/users/:username', function(req, res) {
+  var username = req.params.username
+  var user = getUser(username)
+  user.location = req.body
+  saveUser(username, user)
+  res.end()
+})
 
 var server = app.listen(3000, function() {
   console.log('Server running at http://localhost:' + server.address().port)
 })
 
+
+function getUserFilePath(username) {
+  return path.join(__dirname, 'users', username + '.json')
+}
+
 function getUser(username) {
-  return _.find(users, {username: username})
+  var user = JSON.parse(fs.readFileSync(getUserFilePath(username), {encoding: 'utf8'}))
+  if (!user.name.full) {
+    user.name.full = _.startCase(user.name.first + ' ' + user.name.last)
+  }
+  _.keys(user.location).forEach(function(key) {
+    user.location[key] = _.startCase(user.location[key])
+  })
+  return user
+}
+
+function saveUser(username, user) {
+  var filePath = getUserFilePath(username)
+  fs.unlinkSync(filePath) //delete the file
+  fs.writeFileSync(filePath, JSON.stringify(user, null, 2), {encoding: 'utf8'})
 }
